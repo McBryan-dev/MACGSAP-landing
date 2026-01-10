0eb67b8172262bf61325db2916ff0423ae640535
McBryan <bryanokononfua@gmail.com>
2026-01-10 11:55:11 +0000

    Created the controls

--- src/components/ProductViewer.tsx
+++ src/components/ProductViewer.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import useMacbookStore from '../store';
 import clsx from 'clsx';
+import {Canvas} from '@react-three/fiber';
+import {Box} from '@react-three/drei';
 
 const ProductViewer = () => {
 
@@ -11,7 +13,7 @@ const ProductViewer = () => {
             <h2>Take a closer look</h2>
 
             <div className='controls'>
-                <p className='info'>MacbookPro 16" in {color}</p>
+                <p className='info'>MacbookPro {scale} in {color}</p>
             
                 <div className='flex-center gap-5 mt-5'>
                     <div className='color-control'>
@@ -31,15 +33,20 @@ const ProductViewer = () => {
                     <div className="size-control">
                         <div
                             onClick={() => setScale(0.06)}
-                            
+                            className={clsx(scale === 0.06 ? 'bg-white text-black' : 'bg-transparent text-white')}
                         ><p>14"</p></div>
-                        <div><p>16"</p></div>
+                        <div
+                            onClick={() => setScale(0.08)}
+                            className={clsx(scale === 0.08 ? 'bg-white text-black' : 'bg-transparent text-white')}
+                        ><p>16"</p></div>
                     </div>
                 </div>
 
             </div>
 
-            <p className="text-white text-4xl">Render Canvas</p>
+            <Canvas id="canvas" camera={{position: [0, 2, 5], fov:50, near: 0.1, far: 100 }}>
+                <Box position={[-1, 1, 0]} scale={10 *scale} material-color={color}></Box>
+            </Canvas>
         </section>
     )
 
