97af898dbd50dcdb3be5c25534ae42447fe5821c
McBryan <bryanokononfua@gmail.com>
2026-01-13 23:30:41 +0000

    Next Commit

--- src/components/ProductViewer.tsx
+++ src/components/ProductViewer.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import useMacbookStore from '../store';
 import clsx from 'clsx';
 import {Canvas} from '@react-three/fiber';
-import {Box, OrbitControls} from '@react-three/drei';
+import {OrbitControls} from '@react-three/drei';
 
 import MacbookModel14 from './models/Macbook-14';
+import StudioLights from './StudioLights';
 
 const ProductViewer = () => {
 
@@ -50,7 +51,7 @@ const ProductViewer = () => {
             <Canvas id="canvas" camera={{position: [0, 2, 5], fov:50, near: 0.1, far: 100 }}>
                 {/* <Box position={[0, 0, 0]} scale={10 *scale} material-color={color}></Box> */}
                 
-                <ambientLight intensity={1} />
+                <StudioLights />
 
                 <MacbookModel14 scale={0.06} positiion={[0, 0, 0]} />
                 <OrbitControls enableZoom={false} />
---
+++ src/components/StudioLights.jsx
@@ -0,0 +1,12 @@
+import { Environment } from '@react-three/drei';
+import React from 'react';
+
+const StudioLights = () => {
+    return (
+        <group name="lights">
+            <Environment> </Environment>
+        </group>
+    )
+}
+
+export default StudioLights;
