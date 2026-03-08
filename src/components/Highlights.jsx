59dbb1228b3ab8f44f7ad6915409a3b0c37f16ef
McBryan <bryanokononfua@gmail.com>
2026-03-08 22:23:59 +0000

    Highlights Section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,7 +2,44 @@ import React from 'react';
 
 const Highlights = () => {
     return (
-        <h2>Highlights</h2>
+        <section id="highlights">
+            <h2>There's neve been a better time to upgrade</h2>
+            <h3>Here's what you get with the brand new M4 chip</h3>
+
+            <div className="masonry">
+                <div className='left-column'>
+
+                    <div>
+                        <img src="/laptop.png" alt="Laptop" />
+                        <p>Fly through demanding tasks up to 9.8 times faster.</p>
+                    </div>
+
+                    <div>
+                        <img src="/sun.png" alt="Sun" />
+
+                        <p>A stunning <br />
+                        Liquid Retina XDR <br />
+                        display.</p>
+                    </div>
+                </div>
+
+                <div className='right-column'>
+
+                    <div className='apple-gradient'>
+                        <img src="/ai.png" alt="AI" />
+                        <p>Built for <br /> 
+                        <span>Apple Intelligence</span></p>
+                    </div>
+
+                    <div>
+                        <img src="/battery.png" alt="Battery" />
+                        
+                        <p>Up to 
+                            <span className='green-gradient'>{' '}14 hours{''}</span> more battery life. (Up to 24 hours total.)</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
