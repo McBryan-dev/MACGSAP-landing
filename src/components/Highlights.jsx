81bee1e50a030c2ede5e1b0e0f710264c03fad28
McBryan <bryanokononfua@gmail.com>
2026-03-13 23:06:40 +0000

    Highlights

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import {useGSAP} from '@gsap/react';
-import gsap from gsap;
+import {useMediaQuery} from 'react-responsive';
+import gsap from 'gsap';
 
 const Highlights = () => {
     const isMobile = useMediaQuery({query: '(max-width: 1024px)'});
 
     useGSAP(() => {
-
+        gsap.to(['.left-ccolumn', '.right-column'], {
+            scrollTrigger: {
+                trigger: '#highlights',
+                start: isMobile ? 'bottom bottom' : 'top top'
+            },
+            y: 0,
+            opacccity: 1,
+            stagger: 0.5,
+            duration: 1,
+            ease: 'power1.inOut'
+        })
     })
 
     return (
         <section id="highlights">
             <h2>There's neve been a better time to upgrade</h2>
             <h3>Here's what you get with the brand new M4 chip</h3>
-
+ 
             <div className="masonry">
                 <div className='left-column'>
 
